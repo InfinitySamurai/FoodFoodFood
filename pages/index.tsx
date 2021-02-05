deb8ac932e762acef0a5baea0134b6c087c1572a
Russell Long <russelllongj@gmail.com>
2021-02-05 14:11:11 +1100

    A branch update test

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Hi Elizabeth</title>
+        <title>Create Next aaaaApp</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hey Elizabeth
         </h1>
 
         <p className={styles.description}>
