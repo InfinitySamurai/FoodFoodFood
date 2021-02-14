c5ef03412e53f23ca65d6e56cd0343bc67164d4b
Russell Long <russelllongj@gmail.com>
2021-02-14 12:41:29 +1100

    Add _document to get SSR working I think

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react';
+
 interface IProps {
   Component: any;
   pageProps: any;
 }
 
 function MyApp({ Component, pageProps }: IProps) {
+  useEffect(() => {
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
   return <Component {...pageProps} />;
 }
 
